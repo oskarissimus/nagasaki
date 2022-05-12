ae628ac9b6e147905594ea57be4b6d3752dbf9ad
Oskar Korczak <oskar.korczak@protonmail.com>
2022-05-12 19:30:11 +0200

    jeden test

--- nagasaki/strategy/delta_epsilon_strategy.py
+++ nagasaki/strategy/delta_epsilon_strategy.py
@@ -15,20 +15,48 @@ class DeltaEpsilonStrategy(AbstractStrategy):
         self.price_tolerance = Decimal("100")
         self.amount_tolerance = Decimal("0.000_3")
 
+    def get_actions_ask(self) -> List[Action]:
+        delta = self.delta_adjusted_for_inventory
+        delta_price = self.ref * (1 + delta)
+        epsilon_price = self.top_ask - self.epsilon
+
+        desirable_price = max(delta_price, epsilon_price)
+        desirable_amount = self.total_btc
+        desirable_action = self.ask_action(desirable_price, desirable_amount)
+
+        active_offers = self.state.bitclude.active_offers
+
+        if len(active_offers) == 0:
+            return [desirable_action]
+
+        if len(active_offers) == 1:
+            active_offer = active_offers[0]
+            if self.active_offer_is_within_tolerance(
+                active_offer, desirable_price, desirable_amount
+            ):
+                return []
+
+        actions = self.cancel_all_asks()
+        actions.append(desirable_action)
+        return actions
+
     def get_actions_bid(self):
         logger.info("I'm not even trying to bid.")
         return []
 
-    def get_top_ask(self):
+    @property
+    def top_ask(self):
         return min(self.state.bitclude.orderbook_rest.asks, key=lambda x: x.price).price
 
-    def get_total_btc(self):
+    @property
+    def total_btc(self):
         return (
             self.state.bitclude.account_info.balances["BTC"].active
             + self.state.bitclude.account_info.balances["BTC"].inactive
         )
 
-    def get_ref(self):
+    @property
+    def ref(self):
         return self.state.deribit.btc_mark_usd * self.state.usd_pln
 
     def cancel_all_asks(self):
@@ -47,7 +75,8 @@ class DeltaEpsilonStrategy(AbstractStrategy):
             )
         return actions
 
-    def ask_action(self, price: Decimal, amount: Decimal):
+    @staticmethod
+    def ask_action(price: Decimal, amount: Decimal):
         return Action(
             action_type=ActionTypeEnum.CREATE,
             order=BitcludeOrder(
@@ -68,22 +97,24 @@ class DeltaEpsilonStrategy(AbstractStrategy):
             and abs(active_offer.amount - desireable_amount) < self.amount_tolerance
         )
 
-    def get_delta_adjusted_for_inventory(self, inventory_parameter: Decimal):
+    @property
+    def delta_adjusted_for_inventory(self):
         A = Decimal("-0.0035")
         B = Decimal("0.0055")
-        res = A * inventory_parameter + B
+        res = A * self.inventory_parameter + B
         if res <= 0:
             raise StrategyException(
                 f"Delta is too small for inventory parameter: {res}"
             )
         return res
 
-    def get_inventory_parameter(self):
+    @property
+    def inventory_parameter(self):
         balances = self.state.bitclude.account_info.balances
         total_pln = balances["PLN"].active + balances["PLN"].inactive
         total_btc = balances["BTC"].active + balances["BTC"].inactive
 
-        btc_mark_price_pln = self.get_ref()
+        btc_mark_price_pln = self.ref
 
         total_btc_value_in_pln = total_btc * btc_mark_price_pln
         wallet_sum_in_pln = total_pln + total_btc_value_in_pln
@@ -91,43 +122,3 @@ class DeltaEpsilonStrategy(AbstractStrategy):
             total_btc_value_in_pln / wallet_sum_in_pln
         )  # values from 0 to 1
         return pln_to_sum_ratio * 2 - 1
-
-    def get_actions_ask(self) -> List[Action]:
-        top_ask = self.get_top_ask()
-        total_btc = self.get_total_btc()
-        ref = self.get_ref()
-
-        inventory_parameter = self.get_inventory_parameter()
-        logger.info(f"{inventory_parameter=:.3f}")
-        delta = self.get_delta_adjusted_for_inventory(inventory_parameter)
-        logger.info(f"{delta=:.5f}")
-        delta_price = ref * (1 + delta)
-        epsilon_price = top_ask - self.epsilon
-
-        desirable_price = max(delta_price, epsilon_price)
-        going_for_delta = desirable_price == delta_price
-        logger.info(
-            f"{delta_price=:.2f} {epsilon_price=:.2f} going for {'delta' if going_for_delta else 'epsilon'}"
-        )
-        desirable_amount = total_btc
-
-        if len(self.state.bitclude.active_offers) == 0:
-            desirable_action = self.ask_action(desirable_price, desirable_amount)
-            return [desirable_action]
-
-        elif len(self.state.bitclude.active_offers) == 1:
-            active_offer = self.state.bitclude.active_offers[0]
-            if self.active_offer_is_within_tolerance(
-                active_offer, desirable_price, desirable_amount
-            ):
-                return []
-            else:
-                actions = self.cancel_all_asks()
-                desirable_action = self.ask_action(desirable_price, desirable_amount)
-                actions.append(desirable_action)
-                return actions
-        else:
-            actions = self.cancel_all_asks()
-            desirable_action = self.ask_action(desirable_price, desirable_amount)
-            actions.append(desirable_action)
-            return actions
---
+++ tests/nagasaki/strategy/delta_epsilon_strategy/delta_epsilon_strategy_ask_test.py
@@ -0,0 +1,99 @@
+from decimal import Decimal
+
+import pytest
+
+from nagasaki.clients.bitclude.dto import AccountInfo, Offer, Balance
+from nagasaki.enums.common import ActionTypeEnum, SideTypeEnum
+from nagasaki.models.bitclude import OrderbookRest, OrderbookRestItem, OrderbookRestList
+from nagasaki.state import State, DeribitState, BitcludeState
+from nagasaki.strategy.delta_epsilon_strategy import DeltaEpsilonStrategy
+
+
+@pytest.fixture(name="initialized_state")
+def fixture_initialized_state():
+    btc_price_deribit = 40_000
+    usd_pln = 4
+
+    top_bid_price = 150_000
+    top_ask_price = 170_000
+    top_bid_amount = 1
+    top_ask_amount = 1
+
+    own_bid_price = 160_000
+    own_ask_price = 200_000
+    own_ask_amount = 1
+    own_bid_amount = 1
+
+    active_plns = 100_000
+    active_btcs = 1
+
+    state = State()
+    state.deribit = DeribitState()
+    state.deribit.btc_mark_usd = Decimal(btc_price_deribit)
+    state.usd_pln = Decimal(usd_pln)
+    state.bitclude = BitcludeState(
+        orderbook_rest=OrderbookRest(
+            asks=OrderbookRestList(
+                [
+                    OrderbookRestItem(
+                        price=Decimal(top_ask_price), amount=Decimal(top_ask_amount)
+                    )
+                ]
+            ),
+            bids=OrderbookRestList(
+                [
+                    OrderbookRestItem(
+                        price=Decimal(top_bid_price), amount=Decimal(top_bid_amount)
+                    )
+                ]
+            ),
+        )
+    )
+    state.bitclude.active_offers = [
+        Offer(
+            price=Decimal(own_bid_price),
+            amount=Decimal(own_bid_amount),
+            offertype="bid",
+            currency1="btc",
+            currency2="pln",
+            id_user_open="1337",
+            nr="420",
+            time_open="2020-01-01T00:00:00Z",
+        ),
+        Offer(
+            price=Decimal(own_ask_price),
+            amount=Decimal(own_ask_amount),
+            offertype="ask",
+            currency1="btc",
+            currency2="pln",
+            id_user_open="1337",
+            nr="421",
+            time_open="2020-01-01T00:00:00Z",
+        ),
+    ]
+    state.bitclude.account_info = AccountInfo(
+        balances={
+            "PLN": Balance(active=Decimal(active_plns), inactive=Decimal("0")),
+            "BTC": Balance(active=Decimal(active_btcs), inactive=Decimal("0")),
+        }
+    )
+    state.bitclude.active_offers = []
+    return state
+
+
+def test_ask_bidding_over_epsilon_without_own_orders(initialized_state: State):
+    state = initialized_state
+    state.bitclude.active_offers = []
+    strategy = DeltaEpsilonStrategy(state)
+
+    top_ask_offer = min(state.bitclude.orderbook_rest.asks, key=lambda x: x.price)
+    top_ask = top_ask_offer.price
+    epsilon = Decimal("0.01")
+
+    result_actions = strategy.get_actions_ask()
+    price_to_ask = top_ask - epsilon
+    assert len(result_actions) == 1
+    assert result_actions[0].action_type == ActionTypeEnum.CREATE
+    assert result_actions[0].order.side == SideTypeEnum.ASK
+    assert result_actions[0].order.price == price_to_ask
+    assert result_actions[0].order.amount == Decimal("1")
