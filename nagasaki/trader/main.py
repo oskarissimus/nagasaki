f17060dc1e0ce7018ff2be86b07dcd51a24cb5fc
Adrian Szczepański <kontakt@aszczepanski.pl>
2022-05-24 18:14:03 +0200

    Update main to use new strategies

--- nagasaki/trader/main.py
+++ nagasaki/trader/main.py
@@ -4,8 +4,6 @@ from warnings import filterwarnings
 from apscheduler.schedulers.background import BackgroundScheduler
 from pytz_deprecation_shim import PytzUsageWarning
 
-# from nagasaki.clients.trejdoo_client import TrejdooClient
-
 from nagasaki.clients.bitclude.core import BitcludeClient
 from nagasaki.clients.bitclude_websocket_client import BitcludeWebsocketClient
 from nagasaki.clients.coinbase_client import CoinbaseClient
@@ -17,8 +15,9 @@ from nagasaki.logger import logger
 from nagasaki.state import State
 from nagasaki.state_initializer import StateInitializer
 from nagasaki.state_synchronizer import StateSynchronizer
-from nagasaki.strategy import DeltaEpsilonStrategyAsk
+from nagasaki.strategy import DeltaEpsilonStrategyAsk, DeltaEpsilonStrategyBid
 from nagasaki.strategy.delta_epsilon_strategy.dispatcher import StrategyOrderDispatcher
+from nagasaki.strategy.hedging_strategy import HedgingStrategy
 from nagasaki.strategy_executor import StrategyExecutor
 from nagasaki.trader.trader_app import TraderApp
 from nagasaki.database.database import Base, engine
@@ -51,7 +50,6 @@ if __name__ == "__main__":
     coinbase_client = CoinbaseClient()
     state = State()
 
-    # usd_pln_quoting_client = TrejdooClient()
     yahoo_finance_api_key: str = os.getenv("YAHOO_FINANCE_API_KEY")
     usd_pln_quoting_client = YahooFinanceClient(yahoo_finance_api_key)
 
@@ -65,10 +63,14 @@ if __name__ == "__main__":
 
     state_synchronizer = StateSynchronizer(state, bitclude_client, deribit_client)
 
-    des_ask_dispatcher = StrategyOrderDispatcher(client=bitclude_client, state=state)
-    strategy = DeltaEpsilonStrategyAsk(state=state, dispatcher=des_ask_dispatcher)
+    dispatcher = StrategyOrderDispatcher(client=bitclude_client, state=state)
+    des_ask = DeltaEpsilonStrategyAsk(state=state, dispatcher=dispatcher)
+    des_bid = DeltaEpsilonStrategyBid(state=state, dispatcher=dispatcher)
+    hedging_strategy = HedgingStrategy(state=state, client=deribit_client)
+    strategies = [des_ask, des_bid, hedging_strategy]
+
     strategy_executor = StrategyExecutor(
-        strategies=[strategy],
+        strategies=strategies,
         event_manager=event_manager,
         state=state,
     )
@@ -81,7 +83,6 @@ if __name__ == "__main__":
         bitclude_client=bitclude_client,
         bitclude_websocket_client=bitclude_websocket_client,
         deribit_client=deribit_client,
-        strategy=strategy,
         state=state,
         event_manager=event_manager,
         scheduler=scheduler,
