81fe0076977fb9e4f9505b640fbaebcc526e89a6
Oskar Korczak <oskar.korczak@protonmail.com>
2022-06-02 17:06:07 +0200

    go

--- nagasaki/database/database.py
+++ nagasaki/database/database.py
@@ -3,20 +3,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from nagasaki.settings import Settings
 
+settings = Settings()
 Base = declarative_base()
-SessionLocal = None  # pylint: disable=invalid-name
-engine = None  # pylint: disable=invalid-name
 
+if settings.memory_db:
+    engine = create_engine("sqlite://")
+else:
+    engine = create_engine(settings.connection_string)
 
-def init_db():
-    settings = Settings()
-
-    if settings.memory_db:
-        global engine  # pylint: disable=(global-statement, invalid-name)
-        engine = create_engine("sqlite://")
-    else:
-        global engine  # pylint: disable=(global-statement, invalid-name)
-        engine = create_engine(settings.connection_string)
-
-    global SessionLocal  # pylint: disable=(global-statement, invalid-name)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
