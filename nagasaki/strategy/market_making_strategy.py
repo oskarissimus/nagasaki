21c372d94a14f27e3f0d1ea2592714247c40cff7
Adrian Szczepański <kontakt@aszczepanski.pl>
2022-06-03 20:34:37 +0200

    Remove inventory parameter logic from MMStrategy

--- nagasaki/strategy/market_making_strategy.py
+++ nagasaki/strategy/market_making_strategy.py
@@ -11,18 +11,6 @@ from nagasaki.state import State
 from nagasaki.logger import logger
 
 
-def calculate_btc_value_in_pln(btc: Decimal, price: Decimal) -> Decimal:
-    return btc * price
-
-
-def calculate_inventory_parameter(
-    total_pln: Decimal, total_btc_value_in_pln: Decimal
-) -> Decimal:
-    wallet_sum_in_pln = total_pln + total_btc_value_in_pln
-    pln_to_sum_ratio = total_btc_value_in_pln / wallet_sum_in_pln  # values from 0 to 1
-    return pln_to_sum_ratio * 2 - 1
-
-
 def make_order(price: Decimal, amount: Decimal, side: SideTypeEnum) -> OrderMaker:
     return OrderMaker(
         side=side,
@@ -110,21 +98,6 @@ class MarketMakingStrategy(AbstractStrategy):
             self.state.bitclude.account_info.balances["PLN"].active
             + self.state.bitclude.account_info.balances["PLN"].inactive
         )
-
-    @property
-    def btc_mark_pln(self):
-        return self.state.deribit.btc_mark_usd * self.state.usd_pln
-
-    @property
-    def inventory_parameter(self):
-        balances = self.state.bitclude.account_info.balances
-        total_pln = balances["PLN"].active + balances["PLN"].inactive
-        total_btc = balances["BTC"].active + balances["BTC"].inactive
-
-        total_btc_value_in_pln = calculate_btc_value_in_pln(
-            total_btc, self.btc_mark_pln
-        )
-        return calculate_inventory_parameter(total_pln, total_btc_value_in_pln)
 
     def log_prices(self):
         if self.epsilon_price:
