72ebce467def0a2281ed706cc3bbaf65f30415f0
Adrian Szczepański <kontakt@aszczepanski.pl>
2022-06-11 01:46:09 +0200

    Add missing @inject

--- nagasaki/strategy_executor.py
+++ nagasaki/strategy_executor.py
@@ -1,12 +1,13 @@
 from typing import List
 
-from dependency_injector.wiring import Provide
+from dependency_injector.wiring import Provide, inject
 
 from nagasaki.containers import Application
 from nagasaki.logger import logger
 from nagasaki.strategy.abstract_strategy import AbstractStrategy
 
 
+@inject
 def execute_all_strategies(
     strategies: List[AbstractStrategy] = Provide[
         Application.strategies.strategies_provider
